72b2d64f11bd5690cf9fe9bda4ec10132821c304
emilien.gorjux <emilien.gorjux@gmail.com>
2025-10-07 10:59:12 +0200

    supression espace

--- git/hiit/pretapreter/src/main/java/fr/hiit/pretapreter/entity/config/DataInitializer.java
+++ git/hiit/pretapreter/src/main/java/fr/hiit/pretapreter/entity/config/DataInitializer.java
@@ -24,7 +24,7 @@ public class DataInitializer implements CommandLineRunner {
     @Override
     public void run(String... args) {
         if (repository.count() > 0) {
-            log.info(" Base déjà initialisée ({} matériels)", repository.count());
+            log.info("Base déjà initialisée ({} matériels)", repository.count());
             return;
         }
 
