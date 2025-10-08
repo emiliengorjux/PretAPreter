bffc98c9a7dfe7d04d4fed17ee377ee86419baaf
Eliesse Mosbah <eliesse91@hotmail.fr>
2025-10-08 14:42:49 +0200

    complet dto

--- git/hiit/pretapreter/src/main/java/fr/hiit/pretapreter/service/presentation/dto/EmpruntDto.java
+++ git/hiit/pretapreter/src/main/java/fr/hiit/pretapreter/service/presentation/dto/EmpruntDto.java
@@ -1,5 +1,22 @@
 package fr.hiit.pretapreter.service.presentation.dto;
 
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+
 public class EmpruntDto {
+
+    private String materiel;
+    private String emprunteur;
+    private LocalDate dateEmprunt;
+    private LocalDate dateRetourPrevu;
+    private LocalDate dateRetourEffectif;
+    private String suiviEtatMateriel;
+    private String commentaire;
 
 }
