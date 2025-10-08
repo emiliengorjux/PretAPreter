6dda5e9a6b04859f0f487cef566443dd721bbaa8
Eliesse Mosbah <eliesse91@hotmail.fr>
2025-10-08 16:07:10 +0200

    creation method dans la classe materiel dto to entity materiel

--- git/hiit/pretapreter/src/main/java/fr/hiit/pretapreter/service/presentation/dto/MaterielDto.java
+++ git/hiit/pretapreter/src/main/java/fr/hiit/pretapreter/service/presentation/dto/MaterielDto.java
@@ -1,10 +1,12 @@
 package fr.hiit.pretapreter.service.presentation.dto;
 
+import fr.hiit.pretapreter.service.repository.entity.Materiel;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+
 public class MaterielDto {
     private Long id;
 
@@ -14,4 +16,14 @@ public class MaterielDto {
     private String commentaire;
     private String categorie;
     private String dateAjout;
+
+    public static Materiel toEntityMateriel(MaterielDto materielDto) {
+        Materiel materiel = new Materiel();
+        materiel.setNom(materielDto.getNom());
+        materiel.setReference(materielDto.getReference());
+        materiel.setEtatMateriel(materielDto.getEtatMateriel());
+        materiel.setCommentaire(materielDto.getCommentaire());
+        materiel.setCategorie(materielDto.getCategorie());
+        return materiel;
+    }
 }
