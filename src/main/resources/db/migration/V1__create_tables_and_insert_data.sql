a64e173ca8dc479523ee18f52dfaaf61049a1d5c
Eliesse Mosbah <eliesse91@hotmail.fr>
2025-10-10 10:49:19 +0200

    ajout de mehode emprunt service et correction sql et ajout de testUnitaire emprunt service

--- src/main/resources/db/migration/V1__create_tables_and_insert_data.sql
+++ src/main/resources/db/migration/V1__create_tables_and_insert_data.sql
@@ -22,16 +22,16 @@ CREATE TABLE emprunt (
 );
 
 -- Insertion des données
-INSERT INTO materiel (id, nom, reference, etat_materiel, commentaire, categorie, date_ajout)
+INSERT INTO materiel (nom, reference, etat_materiel, commentaire, categorie, date_ajout)
 VALUES
-(1, 'Ordinateur portable', 'ASUS', 'Bon état', 'RAS', 'Informatique', CURRENT_TIMESTAMP()),
-(2, 'Ecran 24 pouces', 'LG', 'Moyen', 'Quelques rayures', 'Informatique', CURRENT_TIMESTAMP()),
-(3, 'Clavier mécanique', 'Logitech', 'Mauvais', 'A envoyer en réparation', 'Bureautique', CURRENT_TIMESTAMP()),
-(4, 'Switch', 'Nintendo', 'Parfait état', 'RAS', 'Console de jeu', CURRENT_TIMESTAMP());
+('Ordinateur portable', 'ASUS', 'Bon état', 'RAS', 'Informatique', CURRENT_TIMESTAMP()),
+('Ecran 24 pouces', 'LG', 'Moyen', 'Quelques rayures', 'Informatique', CURRENT_TIMESTAMP()),
+('Clavier mécanique', 'Logitech', 'Mauvais', 'A envoyer en réparation', 'Bureautique', CURRENT_TIMESTAMP()),
+('Switch', 'Nintendo', 'Parfait état', 'RAS', 'Console de jeu', CURRENT_TIMESTAMP());
 
-INSERT INTO emprunt (id, materiel_id, emprunteur, date_emprunt, retour_prevu, retour_effectif, suivi_etat_materiel, commentaire)
+INSERT INTO emprunt (materiel_id, emprunteur, date_emprunt, retour_prevu, retour_effectif, suivi_etat_materiel, commentaire)
 VALUES
-(1, 1, 'Didier', '2025-10-07', '2025-10-10', '2025-10-11', 'RAS', 'RAS'),
-(2, 2, 'Yves', '2025-10-07', '2025-10-10', '2025-10-11', 'Bon', 'Matériel en bon état'),
-(3, 3, 'Guy', '2025-10-07', '2025-10-10', '2025-10-11', 'Mauvais', 'Défectueux'),
-(4, 4, 'Michel', '2025-10-07', '2025-10-10', '2025-10-11', 'RAS', 'RAS');
+(1, 'Didier', '2025-10-07', '2025-10-10', '2025-10-11', 'RAS', 'RAS'),
+(2, 'Yves', '2025-10-07', '2025-10-10', '2025-10-11', 'Bon', 'Matériel en bon état'),
+(3, 'Guy', '2025-10-07', '2025-10-10', '2025-10-11', 'Mauvais', 'Défectueux'),
+(4, 'Michel', '2025-10-07', '2025-10-10', '2025-10-11', 'RAS', 'RAS');
