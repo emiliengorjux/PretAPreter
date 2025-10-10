e6c1066af6d8eb7d604ada345560c461e58a797d
Eliesse Mosbah <eliesse91@hotmail.fr>
2025-10-10 11:59:01 +0200

    correrction materielk service

--- src/main/java/fr/hiit/pretapreter/service/MaterielService.java
+++ src/main/java/fr/hiit/pretapreter/service/MaterielService.java
@@ -1,10 +1,14 @@
 package fr.hiit.pretapreter.service;
 
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
+import fr.hiit.pretapreter.service.presentation.dto.MaterielDto;
 import fr.hiit.pretapreter.service.repository.EmpruntRepository;
 import fr.hiit.pretapreter.service.repository.MaterielRepository;
+import fr.hiit.pretapreter.service.repository.entity.Materiel;
 
 @Service
 public class MaterielService {
@@ -25,12 +29,34 @@ public class MaterielService {
         return empruntRepository;
     }
 
+
     public MaterielDto creeMateriel(String nom, String reference, String etatMateriel,
                                     String commentaire, String categorie, LocalDateTime dateAjout) {
         
         Materiel materielR = (Materiel) materielRepository;
 
+        String defectueux = materielR.getEtatMateriel();
+        if (defectueux.equals("Mauvais")) {
+            throw new IllegalStateException("Le matériel est défectueux il ne peut pas être emprunté");
+        }
+
+
+        Materiel materiel = new Materiel();
+        materiel.setNom(nom);
+        materiel.setReference(reference);
+        materiel.setEtatMateriel(etatMateriel);
+        materiel.setCommentaire(commentaire);
+        materiel.setCategorie(categorie);
+        materiel.setDateAjout(dateAjout);
+
+        Materiel savedMateriel = materielRepository.save(materiel);
+
+
+        return MaterielDto.toDtoMateriel(savedMateriel);
+
+
+    }
 
 
 }
-}
+
