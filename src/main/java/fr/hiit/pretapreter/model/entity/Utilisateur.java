03ea0ab0e06af609581e14ed657a1abeb88ff247
Eliesse Mosbah <eliesse91@hotmail.fr>
2025-10-24 11:38:14 +0200

    supression security

--- src/main/java/fr/hiit/pretapreter/model/entity/Utilisateur.java
+++ src/main/java/fr/hiit/pretapreter/model/entity/Utilisateur.java
@@ -22,11 +22,6 @@ public class Utilisateur {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
-    private String motDePasse;
-
-    @Column(nullable = false)
-    private String role;
 
 
     @OneToMany(mappedBy = "utilisateur", cascade = CascadeType.ALL, orphanRemoval = true)
@@ -81,18 +76,5 @@ public class Utilisateur {
 
     public void setEmprunts(Set<Emprunt> emprunts) {
         this.emprunts = emprunts;
-    }
-
-    public String getMotDePasse() {
-        return motDePasse;
-    }
-    public void setMotDePasse(String motDePasse) {
-        this.motDePasse = motDePasse;
-    }
-    public String getRole() {
-        return role;
-    }
-    public void setRole(String role) {
-        this.role = role;
     }
 }
